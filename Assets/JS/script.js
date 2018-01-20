1a19ec064274f6a2bdc7f736a52df967dce70245
Geraldo Perez <gap@geraldoaperez.com>
2018-01-20 15:11:23 -0400

    Initialize firebase

---
+++ Assets/JS/script.js
@@ -0,0 +1,12 @@
+/**
+ * Page functionality for form
+ */
+'use strict'
+
+function writeUserData(userId, name, email, imageUrl) {
+    firebase.database().ref('users/' + userId).set({
+      username: name,
+      email: email,
+      profile_picture : imageUrl
+    });
+}
